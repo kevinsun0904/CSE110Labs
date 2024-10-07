ef2cfecf4036938c68bb7ab7ece48a317d836f86
kevinsun0904 <kevinsun0904@gmail.com>
2024-10-07 15:43:07 -0700

    Exercise 4.3 done

--- Lab2/my-app/src/App.tsx
+++ Lab2/my-app/src/App.tsx
@@ -10,10 +10,36 @@ function App() {
 
   const [currentTheme, setCurrentTheme] = useState(themes.light);
 
+  const [notes, setNotes] = useState(dummyNotesList); 
+  const initialNote = {
+    id: -1,
+    title: "",
+    content: "",
+    label: Label.other,
+  };
+  const [createNote, setCreateNote] = useState(initialNote);
+
+  const [selectedNote, setSelectedNote] = useState<Note>(initialNote);
+
   const onHeartClick = (title: string) => {
     const likeMapCopy = {...likeMap}
     likeMapCopy[title] = !likeMap[title];
     setLikeMap(likeMapCopy);
+  }
+
+  const onNoteSelect = (note: Note) => {
+    setSelectedNote(note);
+    console.log(selectedNote);
+  }
+
+  const createNoteHandler = (event: React.FormEvent) => {
+    event?.preventDefault();
+    setNotes([...notes, createNote]);
+  }
+
+  const onNoteDelete = (deleteNote: Note) => {
+    const filteredNotes = notes.filter(note => note !== deleteNote);
+    setNotes(filteredNotes);
   }
 
   return (
@@ -21,13 +47,32 @@ function App() {
     <div>
       <div className="app-container">
         <ToggleTheme setCurrentTheme={setCurrentTheme}></ToggleTheme>
-        <form>
+        <form onSubmit={createNoteHandler}>
           <div>
-            <input placeholder="Note Title"></input>
+            <input placeholder="Note Title"
+              onChange={(event) =>
+              setCreateNote({ ...createNote, title: event.target.value })}
+              required></input>
           </div>
 
           <div>
-            <textarea></textarea>
+            <textarea
+              onChange={(event) =>
+                setCreateNote({ ...createNote, content: event.target.value })}
+              required>
+            </textarea>
+          </div>
+
+          <div>
+            <select
+              onChange={(event) =>
+                setCreateNote({ ...createNote, label: event.target.value as Label })}
+              required>
+              <option value={Label.personal}>Personal</option>
+              <option value={Label.study}>Study</option>
+              <option value={Label.work}>Work</option>
+              <option value={Label.other}>Other</option>
+            </select>
           </div>
 
           <div>
@@ -35,16 +80,16 @@ function App() {
           </div>
         </form>
         <div className="notes-grid">
-          {dummyNotesList.map((note) => {
+          {notes.map((note) => {
             return (
-              <div key={note.id} className="note-item" style={{ background: currentTheme.background, color: currentTheme.foreground }}>
+              <div key={note.id} className="note-item" onClick={() => onNoteSelect(note)} style={{ background: currentTheme.background, color: currentTheme.foreground }}>
                 <div className="notes-header">
                   <button onClick={() => onHeartClick(note.title)}>{likeMap[note.title] ? "❤️" : "🤍"}</button>
-                  <button style={{ background: currentTheme.foreground, color: currentTheme.background }} >x</button>
+                  <button style={{ background: currentTheme.foreground, color: currentTheme.background }} onClick={() => onNoteDelete(note)} >x</button>
                 </div>
-                <h2> {note.title} </h2>
-                <p> {note.content} </p>
-                <p> {note.label} </p>
+                <h2 contentEditable> {note.title} </h2>
+                <p contentEditable> {note.content} </p>
+                <p contentEditable> {note.label} </p>
               </div>)
           })}
         </div>
